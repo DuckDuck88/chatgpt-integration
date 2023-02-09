20c2e0e0440142f0569de7ea2de7a279da3179ce
yayajmliu <yayajmliu@tencent.com>
2023-02-09 20:38:56 +0800

    后端增加了历史消息接口

--- chatgpt-backend/channel/web/web_channel.py
+++ chatgpt-backend/channel/web/web_channel.py
@@ -18,11 +18,14 @@ class WebChannel(Channel):
         self.table = "web_historys"
         conn = sqlite3.connect(self.db)
         cursor = conn.cursor()
-        cursor.execute(f'''
+        cursor.execute(f"""
           CREATE TABLE IF NOT EXISTS {self.table}
           (_id integer primary key autoincrement,
-            time_now TEXT, query TEXT, reply_text TEXT)
-          ''')
+            time_now TEXT, 
+            query TEXT,
+            reply_text TEXT,
+            ip TEXT)
+            """)
         cursor.close()
 
     def startup(self):
@@ -33,15 +36,33 @@ class WebChannel(Channel):
     def handle(self, request):
         logger.info(request)
         try:
-            query = request.get_json().get("question")
+            question = request.get_json().get("question")
         except Exception as e:
             logger.exception(f'请求异常！{e}')
             return self.failure_reply(e)
-        try:
-            reply_text = self.build_reply_content(query, "openAI", None)
-        except Exception as e:
-            return self.failure_reply(e)
+        reply_text = self.process_question(question)
         return self.success_reply(reply_text)
+
+    def process_question(self, question):
+        conn = sqlite3.connect(self.db)
+        cursor = conn.cursor()
+        time_now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        reply_text = ''
+        try:
+            reply_text = self.build_reply_content(question, "openAI", None)
+        except Exception as e:
+            reply_text = str(e)
+            return str(e)
+        finally:
+            try:
+                cursor.execute(f"INSERT INTO {self.table} VALUES (null, ?, ?, ?)",
+                               (time_now, question, reply_text))
+                conn.commit()
+            except Exception as e:
+                return str(reply_text) + f" \n----\n 此条信息存入数据库失败\n ----\n 原因：{e}"
+            finally:
+                conn.close()
+        return reply_text
 
     def reply(self, msg, receiver):
         """
@@ -57,7 +78,6 @@ class WebChannel(Channel):
         }
 
     def handle_html(self, request):
-        global time_now
         conn = sqlite3.connect(self.db)
         cursor = conn.cursor()
         try:
@@ -69,35 +89,23 @@ class WebChannel(Channel):
         except Exception as e:
             logger.exception(f'请求异常！{e}')
             return self.failure_reply(e)
-        try:
-            time_now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-            cursor.execute(f"SELECT * FROM {self.table} ORDER BY _id DESC")
-            web_history = cursor.fetchall()
-            reply_text = self.build_reply_content(query, "openAI", None)
-        except Exception as e:
-            reply_text = str(e)
-            return self.failure_reply(str(e))
-        finally:
-            try:
-                cursor.execute(f"INSERT INTO {self.table} VALUES (null, ?, ?, ?)", (time_now, query, reply_text))
-                conn.commit()
-            except Exception as e:
-                return render_template(self.html, question=query,
-                                       res=str(str(reply_text) + f'\n----\n此条信息存入数据库失败=> {e}'))
-            finally:
-                conn.close()
+        reply_text = self.process_question(query)
+        web_history = self._get_history()
         return render_template(self.html, question=query, res=str(reply_text), web_history=web_history)
 
     def reply_template(self, request):
         return render_template(self.html, question=0)
 
-    def show_history(self, request):
+    def _get_history(self, ip=None):
         conn = sqlite3.connect(self.db)
         cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM {self.table} ORDER BY _id DESC")
         data = cursor.fetchall()
         logger.info(f'历史记录：{data}')
+        return data
 
+    def show_history(self, request):
+        data = self._get_history()
         return self.success_reply(data)
 
     def failure_reply(self, reply_text, code=400):
